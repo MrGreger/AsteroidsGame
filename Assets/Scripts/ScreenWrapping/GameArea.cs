f6cae814dbb11c5f16772aaba55f1b3f44da83e7
MrGreger <241000g@mail.ru>
2021-01-13 22:21:18 +0300

    Screen wrapping

---
+++ Assets/Scripts/ScreenWrapping/GameArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class GameArea : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider2D _screenBoundsCollider;
+
+    private float _lastCameraAspect;
+
+    private void Update()
+    {
+        if(Camera.main.aspect == _lastCameraAspect)
+        {
+            return;
+        }
+
+        var height = Camera.main.orthographicSize * 2;
+        var width = height * Camera.main.aspect;
+
+        var screenBounds = new Bounds(Camera.main.transform.position, new Vector3(width, height));
+
+        _screenBoundsCollider.transform.localScale = new Vector3(screenBounds.size.x / _screenBoundsCollider.size.x, screenBounds.size.y / _screenBoundsCollider.size.y);
+    }
+}
---
+++ Assets/Scripts/ScreenWrapping/ScreenWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrapper : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _wrapperRoot;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<GameArea>(out var gameArea))
+        {
+            var location = gameArea.transform.InverseTransformPoint(_wrapperRoot.position);
+
+            if (Mathf.Abs(location.y) >= 0.5)
+            {
+                location.y *= -1;
+
+                location.y = Mathf.Clamp(location.y, -0.49f, 0.49f);
+            }
+            if (Mathf.Abs(location.x) >= 0.5)
+            {
+                location.x *= -1;
+
+                location.x = Mathf.Clamp(location.x, -0.49f, 0.49f);
+            }
+
+            _wrapperRoot.position = gameArea.transform.TransformPoint(location);
+        }
+    }
+}
