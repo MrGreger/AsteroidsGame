dc0f9fb1fcb7f3ef28aae50e3edf4610e655e897
MrGreger <241000g@mail.ru>
2021-01-14 00:28:47 +0300

    Basic player death

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    private void Start()
+    {
+        MessageBroker.Default.Receive<OnPlayerDiedEvent>()
+                             .Subscribe(x =>
+                             {
+                                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                             });
+    }
+}
+
